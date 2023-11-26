0b1060691291c7a01793881ed78e758557c6f206
Abhay Sharma <avay.abhay@gmail.com>
2023-11-26 20:10:00 +0530

    RestController, RequestMapping, PathVariable

--- sec - 4-CRUD/crudApp/src/main/java/com/example/crudApp/rest/StudentRestController.java
+++ sec - 4-CRUD/crudApp/src/main/java/com/example/crudApp/rest/StudentRestController.java
@@ -1,15 +1,45 @@
 package com.example.crudApp.rest;
 
+import com.example.crudApp.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/app")
 public class StudentRestController {
 
+    public ArrayList<Student> studentList;
+
+    @PostConstruct
+    public void createList(){
+        studentList = new ArrayList<Student>();
+        studentList.add(new Student("Sandra","Bugg"));
+        studentList.add(new Student("Mendes","Shawn"));
+        studentList.add(new Student("Justin","Timerlake"));
+    }
+
     @GetMapping("/test")
     public String test(){
         return "Hello World!!";
     }
+
+    @GetMapping("/student")
+    public List<Student> getStudents(){
+
+        return studentList;
+    }
+
+    @GetMapping("/student/{studentID}")
+    public Student getStudent(@PathVariable int studentID){
+        return studentList.get(studentID);
+    }
 }
+
+
