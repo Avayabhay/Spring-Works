83bb6744cd3e83047213378edf012e4a33dd7d5e
Abhay Sharma <avay.abhay@gmail.com>
2024-01-27 23:40:18 +0530

    Authorizing API based on roles
    Added SecurityChain and requestMatchers to let users access API based on their roles

--- sec -5 - Spring Security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ sec -5 - Spring Security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,10 +2,13 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -27,9 +30,30 @@ public class DemoSecurityConfig {
         UserDetails susan = User.builder()
                 .username("susan")
                 .password("{noop}susanpass")
-                .roles("EMPLOYEE","MANAGER", "ADMIN")
+                .roles("ADMIN", "EMPLOYEE","MANAGER")
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/employees/**").hasRole("ADMIN")
+                );
+        //since we are using our own request matcher, we need to tell spring that authentication method
+        //use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        //disable CSRF(Cross Site Request Forgery), because it is not needed for stateless REST API calls
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+
     }
 }
