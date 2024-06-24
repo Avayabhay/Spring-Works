885664f0fc6f76e4730f0f628103cd0af0f22b4a
Abhay Sharma <avay.abhay@gmail.com>
2024-06-24 12:52:02 +0530

    setting spring-jpa & jdbc to read & login using database

--- sec-8-Security/spring-security-app/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ sec-8-Security/spring-security-app/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -6,33 +6,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+
+//    Add support for JDBC -> read the login details from the database
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}johnpass")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}marypass")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}susanpass")
-                .roles("EMPLOYEE","MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -56,4 +43,28 @@ public class DemoSecurityConfig {
         .logout(logout -> logout.permitAll());
         return httpSecurity.build();
     }
+
+    //    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}johnpass")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}marypass")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}susanpass")
+//                .roles("EMPLOYEE","MANAGER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
+
 }
